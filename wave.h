6e2ddfa7f6f423ec08b8d0b25f7c861992ce8d9e
ntheod10 <ntheod10@ucy.ac.cy>
2021-11-26 18:43:55 +0200

    wave.h created

---
+++ wave.h
@@ -0,0 +1,51 @@
+#ifndef WAVEH
+#define WAVEH
+
+
+typedef unsigned char byte; 
+typedef unsigned short int word;  
+typedef unsigned int dword; 
+
+
+typedef struct riff_chunk 
+{ 
+    byte chunkId[4];
+    word chunkSize;
+    byte format[4];
+ 
+} RIFF_CHUNK; 
+
+typedef struct fmt_chunk 
+{ 
+    byte subchunk1Id[4];
+    word subchunk1Size;
+    byte audioFormat[2];
+    byte numChannels[2];
+    word sampleRate;
+    word byteRate;
+    byte blockAlign[2];
+    byte bitsPerSample[2];
+ 
+} FMT_CHUNK; 
+
+typedef struct data_chunk 
+{ 
+    byte subchunk2Id[4];
+    word subchunk2Size;
+    byte* data;
+ 
+} DATA_CHUNK; 
+
+
+
+
+typedef struct wave
+{ 
+ RIFF_CHUNK *riff; 
+ FMT_CHUNK *fmt;
+ DATA_CHUNK *data;
+}WAVE; 
+
+
+
+#endif
