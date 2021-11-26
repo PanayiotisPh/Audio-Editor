05082f9df1d004061c5f0ff80cf5d30516b7fa0c
ntheod10 <ntheod10@ucy.ac.cy>
2021-11-26 22:23:16 +0200

    s

--- wave.c
+++ wave.c
@@ -7,7 +7,7 @@ void initialize(WAVE *wave, FILE *fp){
 	wave->fmtChunk = (FMT_CHUNK*)malloc(sizeof(FMT_CHUNK));
     fread(wave->riffChunk,sizeof(RIFF_CHUNK),1,fp);
 	fread(wave->fmtChunk,sizeof(FMT_CHUNK),1,fp);
-	fread(wave->dataChunk,sizeof(byte)*4+sizeof(dword),1,fp);
+	fread(wave->dataChunk,4+sizeof(dword),1,fp);
     wave->dataChunk->data= (byte*) malloc(wave->dataChunk->subchunk2Size);
     fread(wave->dataChunk->data,wave->dataChunk->subchunk2Size,1,fp);
 
@@ -19,7 +19,7 @@ void readWave(WAVE *wave, FILE *fp){
 }
 
 int main(int argc, char *argv[]){
-    printf("%s",argv[1]);
+    printf("%s %ld",argv[1],sizeof(byte));
     fflush(stdout);
     WAVE *wav=NULL;
     wav=(WAVE*)malloc(sizeof(WAVE));
