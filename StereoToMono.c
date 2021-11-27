52ae6314751d19a52d91cf43ac63ced693a0973e
PanayiotisPh <pphotiadis@gmail.com>
2021-11-27 01:40:27 +0000

    ok

--- StereoToMono.c
+++ StereoToMono.c
@@ -4,16 +4,17 @@
 void stereoToMono(char *input, char *output){
     WAVE *wave=NULL;
     wave=(WAVE*)malloc(sizeof(WAVE));
-    initializeFromFile(wave,input); displayInfo(wave);
+    initializeFromFile(wave,input);
     int i;
     int insertCounter = 0;
     WAVE *monoWave = NULL;
     monoWave = (WAVE*) malloc(sizeof(WAVE));
     initializeMonoWave(monoWave, wave);
     for (i=0;i<wave->dataChunk->subchunk2Size; i=i+ (wave->fmtChunk->bitsPerSample)/4){
-        memcpy(&monoWave->dataChunk->data[insertCounter], &wave->dataChunk->data[i], wave->fmtChunk->bitsPerSample/8);
+        memcpy(&monoWave->dataChunk->data[i/2], &wave->dataChunk->data[i], wave->fmtChunk->bitsPerSample/8);
         insertCounter = insertCounter+wave->fmtChunk->bitsPerSample/8;
     }
+    printf("InsertCounter: %d\ti: %d\n", insertCounter,i);
     displayInfo(monoWave);
     printf("\n\n");
     displayInfo(wave);
@@ -50,13 +51,13 @@ void initializeMonoWave(WAVE *monoWave, WAVE *wave){
     monoWave->fmtChunk->numChannels = (word)1;                                      
     monoWave->fmtChunk->sampleRate = wave->fmtChunk->sampleRate;
     monoWave->fmtChunk->bitsPerSample = wave->fmtChunk->bitsPerSample;
-    monoWave->fmtChunk->byteRate = wave->fmtChunk->byteRate;
+    monoWave->fmtChunk->byteRate = wave->fmtChunk->byteRate/2;
     monoWave->fmtChunk->blockAlign = wave->fmtChunk->blockAlign/2;
 
     //Copy DATA SUB-CHUNK  CHANGE LATER ASAP
     strcpy((char *)monoWave->dataChunk->subchunk2Id, (char *)wave->dataChunk->subchunk2Id);
     int OldNumSample = (wave->dataChunk->subchunk2Size) / ((wave->fmtChunk->numChannels) * (wave->fmtChunk->bitsPerSample / 8));
-    monoWave->dataChunk->subchunk2Size = (OldNumSample)*1 * (wave->fmtChunk->bitsPerSample / 8);    //mono subchunk2size
+    monoWave->dataChunk->subchunk2Size = (OldNumSample) * (wave->fmtChunk->bitsPerSample / 8);    //mono subchunk2size
     monoWave->dataChunk->data = (byte *)malloc(monoWave->dataChunk->subchunk2Size * sizeof(byte));
 }
 
