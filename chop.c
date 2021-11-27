b7969adfb84357320996196d2b197dd7ffa3bf30
ntheod10 <ntheod10@ucy.ac.cy>
2021-11-27 18:08:17 +0200

    chopped

--- chop.c
+++ chop.c
@@ -7,17 +7,21 @@ void chop(char **files, int numOfFiles, int start, int end){
         WAVE *chopWave = (WAVE*) malloc(sizeof(WAVE));
         initializeFromFile(origWave,files[i]);
         initializeFromFile(chopWave,files[i]);
-        
+        free(chopWave->dataChunk->data);
+        chopWave->dataChunk->data = (byte *)malloc( (end-start) * sizeof(byte));
+
         int startChunk= start*origWave->fmtChunk->byteRate;
         int endChunk= end*origWave->fmtChunk->byteRate;
-        int chunkPointer;
-        for(chunkPointer=startChunk ; chunkPointer<endChunk ; chunkPointer+= origWave->fmtChunk->sampleRate/8  ){
+        // int chunkPointer;
+        // for(chunkPointer=startChunk ; chunkPointer<endChunk ; chunkPointer+= origWave->fmtChunk->sampleRate/8  ){
             
-        }
+        // }
+        memcpy(chopWave->dataChunk->data,origWave->dataChunk->data[start],end-start);
 
+        exportWave(files[i],chopWave,"chopped-");
         
-
-
+        deallocWave(chopWave);
+        free(chopWave);
         deallocWave(origWave);
         free(origWave);
 
