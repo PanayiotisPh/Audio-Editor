8c674173ee10c9d7cf9fbb6fa79cd4061f922b29
PanayiotisPh <pphotiadis@gmail.com>
2021-11-28 22:26:15 +0000

    ad

--- encode.c
+++ encode.c
@@ -1,6 +1,7 @@
 #include "encode.h"
 
 void encode(char *waveFile, char *textFile){
+    printf("%s\t%s\n",waveFile,textFile);
     char *text = (char*)calloc(sizeof(char),1);
     FILE *fp;
     fp = fopen(textFile, "r");
@@ -11,10 +12,20 @@ void encode(char *waveFile, char *textFile){
         text = (char*)realloc(text, i);
     }
     printf("%s\n", text);
+    getBit(text,0);
 
 }
 int getBit(char *m, int n){
-
+    int byteCal = n/8;
+    int bitCal = (7-(n%8));
+    int i,j;
+    int bit = (m[byteCal]>>bitCal&1);
+    for(i=0;i<strlen(m);i++){
+        //for(j=0;j<)
+        printf("%d",(m));
+    }
+    printf("\n%d\n",bit);
+    return bit;
 
 }
 int *createPermutationFunction(int N, unsigned int systemkey){
@@ -22,6 +33,6 @@ int *createPermutationFunction(int N, unsigned int systemkey){
 
 }
 
-// int main(int argc, char *argv[]){
-//     encode(argv[1],argv[2]);
-// }
+int main(int argc, char *argv[]){
+    encode(argv[1],argv[2]);
+}
