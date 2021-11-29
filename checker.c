c523cbdd7e74f574c273e38129900b2d65700441
PanayiotisPh <pphotiadis@gmail.com>
2021-11-30 00:27:13 +0200

    g

--- checker.c
+++ checker.c
@@ -49,7 +49,7 @@ bool isNumber(char* str){
  */
 bool checkOption(char *argv[]){
         if(strcmp(argv[1],"-list")==0 || strcmp(argv[1],"-mono")==0 || strcmp(argv[1],"-mix")==0 || strcmp(argv[1],"-chop")==0 
-        || strcmp(argv[1],"-reverse")==0 || strcmp(argv[1],"-encodeText")==0 || strcmp(argv[1],"-decodeText")==0 || strcmp(argv[1],"-changeSpeed")==0)
+        || strcmp(argv[1],"-reverse")==0 || strcmp(argv[1],"-encodeText")==0 || strcmp(argv[1],"-decodeText")==0 || strcmp(argv[1],"-changeSpeed")==0 || strcmp(argv[1],"-cut")==0)
             return true;
      return false;    
 }
