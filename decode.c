8209df0df83e6497d17692c057187b11ede7dc3b
ntheod10 <ntheod10@ucy.ac.cy>
2021-11-29 03:06:24 +0200

    asd

--- decode.c
+++ decode.c
@@ -4,14 +4,22 @@
 void decode(char *waveFile, int len, char *textFile){
     WAVE *wave = (WAVE*) malloc(sizeof(WAVE));
     initializeFromFile(wave,waveFile);
-    char *text = (char*)calloc(len,1);
+    byte text[(len+1)];
     FILE *fp;
     fp = fopen(textFile, "wb");
     int *permutation;
     permutation=createPermutationFunction(wave->dataChunk->subchunk2Size/8, KEY);
 
-    int i,u,x;
-    for(i=0; i<(1+len)*8;i++){
-        
-    }
+    int byte,bit,u,x;
+    int textIndex=(1+len)*8;
+    for(byte=0; byte<1+len;byte++)
+        for(bit=7; bit>=0;bit--){
+            x=permutation[bit+byte*8];
+            u= getBit(wave->dataChunk->data,x);
+            text[byte]>>(bit&1) = u;
+        }
+
+    printf("text: %s",text);
+
+
 }
--- wavengine.c
+++ wavengine.c
@@ -6,6 +6,7 @@
 #include "mix.h"
 #include "reverse.h"
 #include "encode.h"
+#include "decode.h"
 
 #ifndef DEBUGGING
 char* GPL= "\n This program is free software: you can redistribute it and/or modify\n it under the terms of the GNU General Public License as published by\n the Free Software Foundation, either version 3 of the License, or\n at your option) any later version.\n \n This program is distributed in the hope that it will be useful,\n but WITHOUT ANY WARRANTY; without even the implied warranty of\n MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n GNU General Public License for more details in the COPYING file.\n\n See license in COPYING file.";
@@ -80,7 +81,7 @@ int main(int argc, char *argv[]){
         
 
         printf("amhere"); 
-        decode(validFiles[0],argv[3],argv[4]);
+        decode(validFiles[0],atoi(argv[3]),argv[4]);
 
     }   
 
