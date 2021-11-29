7654c9246c1d8611e568fc29f627bb42119f7688
PanayiotisPh <pphotiadis@gmail.com>
2021-11-29 17:05:46 +0000

    ad

--- wavengine.c
+++ wavengine.c
@@ -51,34 +51,34 @@ int main(int argc, char *argv[]){
     int *numOfValidFiles;
     numOfValidFiles = malloc(sizeof(int));
     numOfValidFiles[0]=0;
-    char **validFiles= getValidFiles(argv,numOfValidFiles);
+    char **validFiles= getValidFiles(argv,numOfValidFiles); //check if files imported are valid
     char *option= argv[1];
    
-    if(validFiles[0]==NULL){
+    if(validFiles[0]==NULL){       //check if there are valid files
         printf("no valid wave files given\n");
         goto free;
     }    
-    if(strcmp(option,"-list")==0){
+    if(strcmp(option,"-list")==0){      //if option is list execute -list option
         list(validFiles, numOfValidFiles[0]);
         goto free;
     }
-    if(strcmp(option,"-mono")==0){
+    if(strcmp(option,"-mono")==0){      //if option is list execute -mono option
         stereoToMono(validFiles, numOfValidFiles[0]);
         goto free;
     }
-    if(strcmp(option,"-mix")==0){
+    if(strcmp(option,"-mix")==0){       //if option is list execute -mix option
         mixWaves(validFiles,numOfValidFiles[0]);
         goto free;
     }
-    if(strcmp(option,"-chop")==0){
-        if(numOfValidFiles[0]!=1 || !isNumber(argv[3]) || !isNumber(argv[4]) ){
+    if(strcmp(option,"-chop")==0){      //if option is list execute -chop option
+        if(numOfValidFiles[0]!=1 || !isNumber(argv[3]) || !isNumber(argv[4]) ){     //check if arguments are correct (wav file and 2 numbers)
             printf("invalid arguments, please enter only 1 valid .wav file and 2 integers for the seconds' margin\n");
             goto free;
         }
         
-        int start = atoi(argv[3]);
+        int start = atoi(argv[3]);  //convert numbers from int to char
         int end = atoi(argv[4]);
-        if(checkTimings(validFiles[0],start,end)){
+        if(checkTimings(validFiles[0],start,end)){  //check if time to chop are valid
             chop(validFiles,numOfValidFiles[0],start,end);
             
         }else{
@@ -87,22 +87,22 @@ int main(int argc, char *argv[]){
         goto free; 
     }
 
-    if(strcmp(option,"-reverse")==0){
+    if(strcmp(option,"-reverse")==0){   //if option is list execute -reverse option
         reverseWave(validFiles,numOfValidFiles[0]);
         goto free;
     }
 
-    if(strcmp(option,"-encodeText")==0){
-        if(!isTextFile(argv[3]))
+    if(strcmp(option,"-encodeText")==0){    //if option is list execute -encodeText option
+        if(!isTextFile(argv[3]))        //check if the imported file is wav
             goto free;
-        if(!txtFitsWav(validFiles[0],argv[3]))
+        if(!txtFitsWav(validFiles[0],argv[3]))  //check if txt fits wav file
             goto free;
         encode(validFiles[0],argv[3]);
         goto free;
 
     }
 
-    if(strcmp(option,"-decodeText")==0){
+    if(strcmp(option,"-decodeText")==0){        //if option is list execute -decodeText option
         if(!isNumber(argv[3]))
             goto free;
         if(!isTextFile(argv[4]))
